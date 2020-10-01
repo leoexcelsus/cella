77db4158126918ed9c1ffe5cc0b63c7ec1d0cb28
Vitor Santana <santana.vitor@gmail.com>
2020-10-01 15:22:40 -0300

    Add controller rules, method index_search and view index_search with table

---
+++ app/controllers/rules_controller.rb
@@ -0,0 +1,10 @@
+class RulesController < ApplicationController
+
+skip_before_action :authenticate_user!, only: [:index, :show, :index_search]
+
+  def index_search
+    @rules = Rule.all
+    @rules = @rules.sample(3)
+  end
+
+end
