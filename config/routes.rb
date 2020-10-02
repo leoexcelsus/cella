43a0f8f4426bdd2588b1ce80bce59eac0c3fa6c9
Leandro Sousa <leoexcelsus@gmail.com>
2020-10-02 14:43:04 -0300

    restablishing routes operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
+  get "rules/spatial_search/", to: "rules#spatial_search", as: :spatial_search
+
   resources :rules, only: [ :show ]
-  
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "rules/spatial_search/", to: "rules#spatial_search", as: :spatial_search
   resources :rules
 
   get "search_mine/", to: "rules#index_search", as: :index_search
