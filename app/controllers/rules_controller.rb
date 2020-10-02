f7975c25b5b0b6a8805448a9f083222d37babf56
Leandro Sousa <leoexcelsus@gmail.com>
2020-10-02 16:19:52 -0300

    first draft

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -14,6 +14,19 @@ class RulesController < ApplicationController
   def show
   end
 
+  def spatial_search
+
+  end
+
+  def spatial_query
+    @searched_polygon = "POLYGON((-52.11614218435184 -6.411085727796378,-59.58442484383378 -10.843968821105001,-53.43407441837804 -19.40872775103672,-46.05365390783119 -11.96456032658079,-52.11614218435184 -6.411085727796378))"
+    @searched_industry = "rodovia"
+    @sql1 = "SELECT * FROM polygons WHERE ST_Intersects('#{@searched_polygon}', polygons.geography)"
+    @array_polygons = ActiveRecord::Base.connection.execute(sql1)
+    @filter1 = @array_polygons.rules
+
+    @results
+  end
 
   private
   # Use callbacks to share common setup or constraints between actions.
@@ -21,7 +34,5 @@ class RulesController < ApplicationController
     @rule = Rule.find(params[:id])
   end
 
-  def spatial_search
-  end
 
 end
