210fa6d2961ed5f35219c16b0d3c72d831afadda
Leandro Sousa <leoexcelsus@gmail.com>
2020-10-12 11:22:25 -0300

    ajax resquest now gets fast response from server, who now sends a WKT polygon, which is quickly drawn in map via Wicket.js

--- app/javascript/components/draw_polygon.js
+++ app/javascript/components/draw_polygon.js
@@ -3,7 +3,17 @@ const drawPolygon = () => {
   const mapElement = document.getElementById('map-index');
 
   if (mapElement) {
-    var mapContainer = L.map(mapElement).setView([-15.5, -48], 5);
+    var mapContainer = L.map(mapElement, {zoomControl: false, scrollWheelZoom: false})
+    .setView([-15.5, -48], 5)
+    .setMaxBounds([[-38.7, -85.8],[11.7, -21.7]]);
+
+    mapContainer.on('click', function() {
+      if (mapContainer.scrollWheelZoom.enabled()) {
+        mapContainer.scrollWheelZoom.disable();
+      } else {
+        mapContainer.scrollWheelZoom.enable();
+      }
+    });
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGVvZXhjZWxzdXMiLCJhIjoiY2tleWNxcnB2MGFzejJwcXQwdGIwMmR2byJ9.CbC1R_EI0AICNAxaZSvyeg', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -28,21 +38,20 @@ const drawPolygon = () => {
       // to do: change the respective Polygons Controller action so as to pass a WKT (stringfied polygon)
       // this way is too slow for big complex polygons.
       fetch(`/polygons/${selectPolygon.value}`)
-        .then(response => console.log(response));
-        return;
-        // .then(response => JSON.parse(response.polygons))
-        // .then((data) => {
-        //   Object.keys(data).forEach( key => {
-        //     // instantiate a Wicket object. Wicket is a JS library that translates
-        //     // Well Known Text into Leaflet layers.
-        //     let wkt = new Wkt.Wkt();
-        //     wkt.read(jsonWkts[key]);
-        //     let wktToLeafletObejct = wkt.toObject();
-        //     polygonsGroup.addLayer(wktToLeafletObejct);
-        //   });
-        //   polygonsGroup.addTo(mapContainer);
-        //   mapContainer.fitBounds(polygonsGroup.getBounds());
-        // });
+        .then(response => response.json())
+        .then(json => JSON.parse(json.polygons))
+        .then((data) => {
+          Object.keys(data).forEach( key => {
+            // instantiate a Wicket object. Wicket is a JS library that translates
+            // Well Known Text into Leaflet layers.
+            let wkt = new Wkt.Wkt();
+            wkt.read(data[key]);
+            let wktToLeafletObejct = wkt.toObject();
+            polygonsGroup.addLayer(wktToLeafletObejct);
+          });
+          polygonsGroup.addTo(mapContainer);
+          mapContainer.fitBounds(polygonsGroup.getBounds());
+        });
     });
   };
 };
